77d06e23d12f4eb7a146abfca2a0b620d2c9e818
Norifumi Ogawa <n@ogaworks.com>
2022-09-02 21:44:44 +0900

    schedules.js読み込みのエラー対応

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 const { VueLoaderPlugin } = require('vue-loader')
 
 environment.plugins.prepend(
@@ -14,12 +23,5 @@ environment.loaders.prepend('vue', {
   }]
 })
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+
 module.exports = environment
