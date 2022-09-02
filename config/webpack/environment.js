5608ecf19593e44b696f7e835337ad3d4b7aafac
NorifumiOgawa <n@ogaworks.com>
2022-09-02 15:47:36 +0900

    Vue3を導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
+const { VueLoaderPlugin } = require('vue-loader')
+
+environment.plugins.prepend(
+  'VueLoaderPlugin',
+  new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue', {
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
+})
+
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
