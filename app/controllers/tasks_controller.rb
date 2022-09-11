adf7e3fdfb2afc7dd3cab2199fdba80e4cc2127c
Norifumi Ogawa <n@ogaworks.com>
2022-09-12 00:05:40 +0900

    taskの編集画面追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i[ edit update ]
+
+  def edit
+  end
+
+  def update
+    if params[:status] == '0'
+      @task.done_at = ''
+    elsif params[:status] == '1'
+      @task.done_at = DateTime.now
+    end
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to schedule_url(@task.schedule), notice: "Task was successfully updated." }
+        format.json { render :show, status: :ok, location: @task }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @task.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:done_memo, :done_at)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :schedules
+  resources :tasks, only: [:update, :edit]
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks",
     registrations: 'users/registrations'
