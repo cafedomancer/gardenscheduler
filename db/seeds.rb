ce6cce685a5629cd14230ad2a8a4356162a1d52d
Norifumi Ogawa <n@ogaworks.com>
2022-09-14 20:50:18 +0900

    seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'n@ogaworks.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+User.create!(email: 'admin@gardenscheduler.com', password: 'password', password_confirmation: 'password')
+Variety.create!(
+  [
+    {
+      name: 'ハイビスカス',
+      user_id: 1
+    },
+    {
+      name: 'バナナ',
+      user_id: 1
+    },
+    {
+      name: 'コーヒー',
+      user_id: 1
+    },
+    {
+      name: 'マニラヤシ',
+      user_id: 1
+    },
+    {
+      name: 'パッションフルーツ',
+      user_id: 1
+    },
+    {
+      name: 'ゴーヤー',
+      user_id: 1
+    },
+    {
+      name: '姫高麗芝',
+      user_id: 1
+    }
+  ]
+)
