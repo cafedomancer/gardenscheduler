bc8e9a10b66e526fb91a8dc2fb0e9f76744fc3ce
Norifumi Ogawa <n@ogaworks.com>
2022-11-12 10:16:10 +0900

    タスクの編集権限を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ edit update ]
+  before_action :set_task, only: %i[edit update]
+  before_action :check_owner, only: %i[edit update destroy]
 
   def edit; end
 
@@ -26,11 +27,16 @@ class TasksController < ApplicationController
   end
 
   private
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    def task_params
-      params.require(:task).permit(:done_memo, :done_at, :done_image1)
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:done_memo, :done_at, :done_image1)
+  end
+
+  def check_owner
+    redirect_to root_path, notice: '権限が有りません。' if !user_signed_in? || current_user.id != @task.user_id
+  end
 end
--- lib/tasks/push_message.rake
+++ lib/tasks/push_message.rake
@@ -2,9 +2,7 @@ namespace :push_message do
   desc 'LINE pushメッセージの送信'
   task notification: :environment do
     tasks = Task.where('date = ? and done_at IS NULL', Date.current)
-    p tasks
     tasks.each do |task|
-      p task
       next if task.user.notification_time.hour != Time.current.hour
 
       message_body = "こんにちは！今日は#{task.schedule.variety.name}(#{task.schedule.name})の「#{task.name}(#{task.plan_memo})」の作業日です！"
@@ -17,9 +15,7 @@ namespace :push_message do
         config.channel_secret = Rails.application.credentials.LINE_BOT_CHANNEL_SECRET
         config.channel_token = Rails.application.credentials.LINE_BOT_CHANNEL_TOKEN
       }
-      p client
       response = client.push_message(task.user.uid, message)
-      p response
     end
   end
 end
