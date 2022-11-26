1d81d3069bfdcb8e06c7608bcfd2edaa85085210
Norifumi Ogawa <n@ogaworks.com>
2022-11-26 16:22:00 +0900

    capybaraでJSのテスト対応調整中

--- spec/requests/schedules_spec.rb
+++ spec/requests/schedules_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Schedules', type: :request do
     before do
       @user = FactoryBot.create(:user)
       @variety = FactoryBot.create(:variety, id: 1)
-      @schedule = FactoryBot.create(:schedule, id: 1, user_id: @user.id, variety_id: @variety.id)
+      @schedule = FactoryBot.create(:schedule, id: 1, user_id: @user.id, variety_id: @variety.id, prefecture_id:47)
       @task = FactoryBot.create(:task, schedule_id: @schedule.id, user_id: @user.id, name: '酸性土壌を作れ')
     end
 
@@ -74,6 +74,61 @@ RSpec.describe 'Schedules', type: :request do
         get '/schedules/1/edit'
         expect(response.body).to include 'スケジュール編集'
       end
+    end
+
+    context 'スケジュールのCRUD' do
+      it 'スケジュールをCREATEできる' do
+        FactoryBot.create(:variety, id: 2, name: 'そこらへんの草')
+
+        sign_in @user
+        visit root_path
+        expect(page).to have_content '今後の予定作業'
+        expect(page).to have_content '作成'
+        click_link('作成')
+
+        expect(page).to have_content 'スケジュール作成'
+        fill_in('schedule[name]', with: 'テストの畑')
+        select('和歌山県', from: 'schedule[prefecture_id]')
+        select('そこらへんの草', from: 'schedule[variety_id]')
+        click_on('登録する')
+
+        expect(page).to have_content 'スケジュールを登録しました。'
+        expect(page).to have_content 'そこらへんの草(テストの畑)@和歌山県'
+      end
+
+      it 'スケジュールをEDIT/UPDATEできる' do
+        FactoryBot.create(:variety, id: 3, name: 'あっちの花')
+        sign_in @user
+        visit schedules_path
+        expect(page).to have_content 'みんなの栽培スケジュール'
+        expect(page).to have_content 'とうだいのひまわり@沖縄県'
+        click_link('とうだいのひまわり@沖縄県')
+        click_link('編集')
+        expect(page).to have_content 'スケジュール編集'
+
+        fill_in('schedule[name]', with: '試験中の畑')
+        select('三重県', from: 'schedule[prefecture_id]')
+        select('あっちの花', from: 'schedule[variety_id]')
+        click_on('更新する')
+
+        expect(page).to have_content 'スケジュールを更新しました。'
+        expect(page).to have_content 'あっちの花(試験中の畑)@三重県'
+      end
+
+      it 'スケジュールをDESTROYできる', js: true do
+        sign_in @user
+        visit schedules_path
+        expect(page).to have_content 'みんなの栽培スケジュール'
+        expect(page).to have_content 'とうだいのひまわり@沖縄県'
+        click_link('とうだいのひまわり@沖縄県')
+        click_link('削除')
+        page.accept_confirm do
+          click_link 'OK'
+        end
+        expect(page).to have_content 'スケジュールを削除しました。'
+        expect(page).not_to have_content 'あっちの花(試験中の畑)@三重県'
+      end
+
     end
   end
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,32 +1,13 @@
 require 'capybara/rspec'
 
-Capybara.register_driver :remote_chrome do |app|
-  url = 'http://chrome:4444/wd/hub'
-  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions' => {
-      'args' => [
-        'no-sandbox',
-        'headless',
-        'disable-gpu',
-        'window-size=1680,1050'
-      ]
-    }
-  )
-  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
-end
-
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 3000
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 3000
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+    driven_by :selenium, using: :headless_chrome, options: {
+      browser: :remote,
+      url: ENV.fetch("SELENIUM_DRIVER_URL"),
+      desired_capabilities: :chrome
+    }
+    Capybara.server_host = 'web'
+    Capybara.app_host='http://web'
   end
 end
